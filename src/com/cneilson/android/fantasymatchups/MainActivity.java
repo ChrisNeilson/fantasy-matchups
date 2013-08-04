da2b0a7608e34f1f6a9705d6ac6ff17af065125b
ChrisNeilson <chris_neilson@hotmail.com>
2013-08-04 15:06:16 -0400

    login page

--- src/com/cneilson/android/fantasymatchups/MainActivity.java
+++ src/com/cneilson/android/fantasymatchups/MainActivity.java
@@ -1,15 +1,18 @@
 package com.cneilson.android.fantasymatchups;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login_android);
+        setUpSiteSpinner();
     }
 
 
@@ -18,6 +21,14 @@ public class MainActivity extends Activity {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
+    }
+
+    public void setUpSiteSpinner() {
+        Spinner site_spinner = (Spinner) findViewById(R.id.site_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.site_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        site_spinner.setAdapter(adapter);
     }
     
 }
